a0d32227807bbf330a93e1cbd933730ad3e3b36b
Billy Shi <zs371@cam.ac.uk>
2022-01-02 00:04:16 +0000

    Updated truss windows for first two steps

--- Indeterminate_truss.py
+++ Indeterminate_truss.py
@@ -13,23 +13,20 @@ Date: Dec 29, 2021
 import numpy as np
 import tkinter as tk
 from tkinter import simpledialog
-fields = 'Last Name', 'First Name', 'Job', 'Country'
-def bars():
-    window = tk.Tk()
-    window.withdraw()
-    bars = simpledialog.askstring(title="Number of Bars", prompt="How many bars are there?")
-    assert int(bars)
-    bars = int(bars)
-    return bars
 
-def fetch(entries):
-    for entry in entries:
-        field = entry[0]
-        text  = entry[1].get()
-        print('%s: "%s"' % (field, text)) 
+# generalize an error window
+def error():
+    root = tk.Tk()
+    root.title("Sorry! Your inputs are not valid!")
+    root.geometry('300x50')
+    q_button = tk.Button(root, text='Try Again', command=root.quit)
+    q_button.pack(side=tk.BOTTOM, padx=5, pady=5)
+    root.mainloop()
 
-def makeform(root, fields):
-    entries = []
+# Step 1: obtain number of bars and redundancies
+fields = 'Num of Bars', 'Num of Redundancies'
+def window1(root, fields):
+    entries1 = []
     for field in fields:
         row = tk.Frame(root)
         lab = tk.Label(row, width=15, text=field, anchor='w')
@@ -37,21 +34,54 @@ def makeform(root, fields):
         row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
         lab.pack(side=tk.LEFT)
         ent.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
-        entries.append((field, ent))
-    return entries
+        entries1.append((field, ent))
+    return entries1
+bar_num = 0
+redun = 0
+def fetch1(entries1):   # fetch data from entries
+    global bar_num
+    global redun
+    bar_num = entries1[0][1].get()
+    redun = entries1[1][1].get()
 
-if True:
-    print("We are ready to solve your indeterminate truss")
-    print("Please calculate and enter values as prompted")
-    root = tk.Tk()
-    ents = makeform(root, fields)
-    root.bind('<Return>', (lambda event, e=ents: fetch(e)))   
-    b1 = tk.Button(root, text='Show',
-                  command=(lambda e=ents: fetch(e)))
-    b1.pack(side=tk.LEFT, padx=5, pady=5)
-    b2 = tk.Button(root, text='Quit', command=root.quit)
-    b2.pack(side=tk.LEFT, padx=5, pady=5)
-    root.mainloop()
+print("We are ready to solve your indeterminate truss")
+print("Please calculate and enter values in the window")
+root = tk.Tk()
+root.title("Number of Bars and Redundancies")
+ents = window1(root, fields)
+s_button = tk.Button(root, text='Store', command=(lambda e=ents: fetch1(e)))
+q_button = tk.Button(root, text='Next', command=root.quit)
+s_button.pack(side=tk.LEFT, padx=5, pady=5)
+q_button.pack(side=tk.LEFT, padx=5, pady=5)
+root.mainloop()
+if int(bar_num) <= 1 or int(redun) <= 0:
+    error()
+    quit()
 
+# Step 2: ask for lengths of each bar
+def window2(root, bar_num):
+    entries2 = []
+    for i in range(1, int(bar_num)+1):
+        row = tk.Frame(root)
+        text = "Bar" + str(i)
+        lab = tk.Label(row, width=15, text=text, anchor='w')
+        ent = tk.Entry(row)
+        row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+        lab.pack(side=tk.LEFT)
+        ent.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
+        entries2.append(ent)
+    return entries2
+bars_len = []
+def fetch2(entries2):   # fetch entered bar lengths
+    global bars_len
+    bar_len = entries2
 
-
+print("Thanks. We will now need more information")
+root = tk.Tk()
+root.title("Enter Lengths of Bars")
+ents = window2(root, bar_num)
+s_button = tk.Button(root, text='Store', command=(lambda e=ents: fetch2(e)))
+q_button = tk.Button(root, text='Next', command=root.quit)
+s_button.pack(side=tk.LEFT, padx=5, pady=5)
+q_button.pack(side=tk.LEFT, padx=5, pady=5)
+root.mainloop()
