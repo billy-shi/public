a36ab6822139e3800e5f64c30ec78b15af361c0f
Billy Shi <zs371@cam.ac.uk>
2022-01-30 23:08:20 +0000

    dice sums pushed

---
+++ probability_calc.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+
+"""
+This programme solve a series of probability questions
+
+Version 1.0
+
+Author: Billy Shi
+Date: Jan 22, 2022
+"""
+
+import random
+import numpy
+import matplotlib.pyplot as plt
+
+def main():
+    """
+    Main execution function with terminal interface
+    """
+
+    menu = input("Click enter to view ")
